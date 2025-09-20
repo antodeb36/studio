e8662af0c75ee7ddc1e58aab85aa946b64daf205
Anto <antodeb36@gmail.com>
2025-09-20 17:41:26 +0000

    Add logo name : E-Stock

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = {
-  title: 'ImageFlow',
+  title: 'E-Stock',
   description: 'Free stock image website with AI-powered tagging.',
 };
 
