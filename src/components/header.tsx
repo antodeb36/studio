f8f77c19347204f6edefeb2889f9f82d555a2648
Anto <antodeb36@gmail.com>
2025-09-21 12:12:39 +0000

    logo side er sate lege ase please gap logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -43,9 +43,11 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
-        <Link href="/" className="mr-auto">
-          <Image src="https://drive.google.com/uc?export=download&id=1AG06J7S8dbVK-r27USyHKQd2sU5OQYR9" alt="E-Stock logo" width={120} height={40} />
-        </Link>
+        <div className="mr-auto">
+          <Link href="/">
+            <Image src="https://drive.google.com/uc?export=download&id=1AG06J7S8dbVK-r27USyHKQd2sU5OQYR9" alt="E-Stock logo" width={120} height={40} />
+          </Link>
+        </div>
         <div className="flex items-center justify-end space-x-4">
           {pathname === '/' && (
             <Suspense>
