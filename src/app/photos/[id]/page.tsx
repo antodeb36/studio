2284fd72993ac99b7a74c90e9fac494bb8fec02b
Anto <antodeb36@gmail.com>
2025-09-23 03:50:58 +0000

    <script async="async" data-cfasync="false" src="//pl27698582.revenuecpmg

--- src/app/photos/[id]/page.tsx
+++ src/app/photos/[id]/page.tsx
@@ -6,6 +6,7 @@ import { Badge } from "@/components/ui/badge";
 import { ImageDetailsClient } from "./image-details-client";
 import { ImageCard } from "@/components/image-card";
 import { Separator } from "@/components/ui/separator";
+import { AdBanner } from "@/components/ad-banner";
 
 export default function PhotoPage({ params }: { params: { id: string } }) {
   const image = getImageById(params.id);
@@ -37,6 +38,7 @@ export default function PhotoPage({ params }: { params: { id: string } }) {
                   priority
                 />
               </div>
+              <AdBanner />
             </div>
             <div className="lg:col-span-1">
               <div className="sticky top-24">
---
+++ src/components/ad-banner.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Script from 'next/script';
+
+export function AdBanner() {
+  return (
+    <div className="my-4 flex justify-center">
+      <div id="container-f2a247c5464320760236868ceca4079d"></div>
+      <Script
+        async
+        data-cfasync="false"
+        src="//pl27698582.revenuecpmgate.com/f2a247c5464320760236868ceca4079d/invoke.js"
+      />
+    </div>
+  );
+}
